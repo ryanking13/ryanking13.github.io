78eb3c39682c44094b028a6c7324bc023a687073
hyoputer <ajtwlsalsdl0@gmail.com>
2021-02-07 09:42:36 +0900

    change to Fuse.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -4,15 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const searchWrapper = document.querySelector("aside[role=search]");
     const searchResultElement = searchWrapper.querySelector(".search-results");
     const searchInput = searchWrapper.querySelector("input");
-
-    function trimmerEnKo(token) {
-        return token.update(function (str, metadata) {
-            return str
-                .replace(/^[^\w가-힣]+/, '')
-                .replace(/[^\w가-힣]+$/, '');
-        })
-    };
     
+    var fuseOptions = {
+        shouldSort: true,
+        includeMatches: true,
+        threshold: 0.0,
+        tokenize: true,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            { name: "title", weight: 0.8 },
+            { name: "contents", weight: 0.5 },
+            { name: "tags", weight: 0.3 },
+            { name: "categories", weight: 0.3 }
+        ]
+    };
+
     function categories(categories, searchString) {
         let tagHTML = (categories.split(" ; ") || [])
             .filter(function (i) {
@@ -53,33 +62,12 @@ document.addEventListener("DOMContentLoaded", function () {
     axios.get("/search-data")
         .then(function (result) {
             const searchContent = result.data;
-            const searchIndex = lunr(function () {
-                this.pipeline.reset();
-                this.pipeline.add(
-                    trimmerEnKo,
-                    lunr.stopWordFilter,
-                    lunr.stemmer
-                );
-                this.ref("id")
-                this.field("content");
-                this.field("categories");
-                this.field("title");
-
-                Array.from(result.data).forEach(function (doc) {
-                    this.add(doc)
-                }, this)
-            })
+            var fuse = new Fuse(searchContent, fuseOptions);
             searchInput.removeAttribute("disabled");
             searchInput.addEventListener("keyup", function (e) {
                 let searchString = e.target.value;
-                if (searchString && searchString.length > 2) {
-                    try {
-                        searchResults = searchIndex.search(searchString);
-                    } catch (err) {
-                        if (err instanceof lunr.QueryParseError) {
-                            return;
-                        }
-                    }
+                if (searchString && searchString.length >= 2) {
+                    searchResults = fuse.search(searchString);
                 } else {
                     searchResults = [];
                 }
@@ -87,15 +75,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (searchResults.length > 0) {
                     searchResultElement.innerHTML = searchResults.map(function (match) {
                         let item = searchContent.find(function (e) {
-                            return e.id == parseInt(match.ref);
+                            return e.id == parseInt(match.item.id);
                         });
+                        
                         return "<li>" +
                             "<h4 title='field: title'><a href='" + item.url + "'>" + mark(item.title, searchString) + "</a></h4>" +
                             "<p class='summary' title='field: content'>" +
                             mark((item.content.length > 200 ? (item.content.substring(0, 200) + "...") : item.content), searchString) +
                             "</p>" +
                             "<p class='tags' title='field: tag'>" + categories(item.categories, searchString) + "</p>" +
-                            "<a href='" + item.url + "' title='field: url'>" + mark(item.url, searchString) + "</a>" +
+                            "<a href='" + item.url + "' title='field: url'>" + item.url + "</a>" +
                             "</li>";
                     }).join("");
                 } else {
